24dd4017338ffc5320d2b877c024349f2f6d3731
Phillip Henault <phenault@live.com>
2021-01-03 10:43:51 -0600

    Added example file.

---
+++ tests/Spottigus.Tests.Helpers/Example.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace Spottigus.Tests.Helpers.Example
+{
+    public class ExampleTest : SqliteInMemoryTest
+    {
+        //[Fact] Un-comment me if you want to run this as a test.
+        public void MethodName_StateUnderTest_ExpectedBehavior()
+        {
+            //Arrange
+            SomeDbContext mockDbContext = CreateMockDbContext();
+        
+            //Act
+        
+        
+            //Assert
+        
+        
+        }
+
+
+        private SomeDbContext CreateMockDbContext()
+        {
+            var dbConnection = CreateInMemoryDatabase();
+            var options = new DbContextOptionsBuilder().UseSqlite(dbConnection).Options;
+            SomeDbContext mockDbContext = new SomeDbContext(options);
+            mockDbContext.Database.EnsureCreated();
+            return mockDbContext;
+        }
+
+        private class SomeDbContext : DbContext
+        {
+            public SomeDbContext(DbContextOptions options) : base (options)
+            {
+                
+            }
+        }
+    }
+}
